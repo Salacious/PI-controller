545caa4aed5d761d78adb4d7fc9e19ff65f3a88f
Nokukhanya Khwela <u29617538@tuks.co.za>
2012-05-07 00:08:30 +0200

    third order simulated using the exponential matrix

--- thirdorder.py
+++ thirdorder.py
@@ -1,62 +1,59 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from scipy import linalg
-import matplotlib.pyplot as plt
-from operator import itemgetter
-import pareto
-from overshoot import*
-from risetime import*
 from plotgraphs import*
-from response import* 
+from ZN import*
 
-tfinal = 10
+tfinal = 100 # simulation period
 dt = 0.2
 t = np.arange(0, tfinal, dt)
 entries = len(t)
-num = 10
+num = 100 # number of tuning constant sets
 x = np.zeros((entries,num))
 k_c = np.zeros(num)
 t_i = np.zeros(num)
 por = np.zeros(num)
 tr = np.zeros(num)
 aa = np.random.rand(num,2)
-k_c = 100*aa[:, 0]
-t_i = 10*aa[:, 1]
-# coefficients of the transfer function
+k_c =10*aa[:, 0]
+t_i = aa[:, 1] + 0.5
+# coefficients of the transfer function Gp = kp/(s^3 + As^2 + Bs +C)
 A =3
 B = 3
-C = 2
-kp =0.125
-SP = 1
-a = 0
-count = 0
+C = 1
+kp =0.125 
+SP = 1 
+#k_c = np.linspace(2,20,num)
+# Relatiopnship btwn kc and Ti obtained through the direct substitution method
+#t_i = np.abs(-kp*k_c/(((kp*k_c + C)/A)**2 - (kp*k_c + C)/A))
+kczn ,tizn  =ZN(A,B,C,kp) # Ziegler-Nichols settings via function ZN
 
 for k in range(0,num):
-    kc =k_c[k]
-    ti = t_i[k]
+    if k==num-1:
+        kc = kczn
+        ti = tizn    
+    else:    
+        kc =k_c[k]
+        ti = t_i[k]
     firstrow =[(ti/(kp*kc)*(C + (kp*kc))), (ti/(kp*kc))*B, (ti/(kp*kc))*A, (ti/(kp*kc))] 
-    Adot = [firstrow,[-1 ,0,0,0],[0,-1,0,0],[0, 0 ,-1,0]]
-    cmat = -1*np.eye(4)
-    Amat = np.dot((linalg.inv(Adot)),cmat)
+    mat = [firstrow,[-1 ,0,0,0],[0,-1,0,0],[0, 0 ,-1,0]]
+    Amat = -1*linalg.inv(mat)
     rootsA = np.array(linalg.eigvals(Amat))
-    Bmat = np.dot(linalg.inv(Adot),[[1],[0] ,[0],[0]])
+    Bmat = np.dot(linalg.inv(mat),[[1],[0] ,[0],[0]])
     Xo = -1*np.dot(linalg.inv(Amat),Bmat*SP)
+    #print rootsA
     for i in range(0,entries):
-        At = linalg.expm2(Amat*t[i])
-        X = np.dot(((linalg.expm2(At))),Xo)
+        X = np.dot(1 - linalg.expm2(Amat*t[i]), Xo)
         x[i,k] = X[0]
-        if X[0]> 50:
+        if X[0] > 3*SP:
             print 'the system is unstable, overshoot ratio and rise time undefined'
-            x[i,k] = None
+            x[:,k] = None
             break
-        
-        count = count + 1 
-
+    k_c[k] = kc
+    t_i[k] = ti
 kc = k_c
 ti = t_i
-x = np.transpose(x)
-por = overshoot(x,SP,num,entries)
-tr = risetime(x,SP,num,entries,t)
-x = np.transpose(x)
-fig,ax1,ax2,ax3,line2,goodpoints = plotgraphs(k_c,t_i,tr,por,x,num,tfinal)
-fig = response(ax1,ax2,ax3,x,por,tr,kc,ti,goodpoints,line2,tfinal,dt,fig)
+x = x.T
+fig = plotgraphs(kc,ti,x,num,entries,t,tfinal,dt,SP)
+
+
