136a39aaae46a48ebf39f4934f79a94cfed77a76
Nokukhanya Khwela <u29617538@tuks.co.za>
2012-05-14 00:47:49 +0200

    third order with stability limit line

--- numerical.py
+++ numerical.py
@@ -2,24 +2,18 @@
 
 import numpy as np
 from numpy import linalg as LA
-import matplotlib.pyplot as plt
-from operator import itemgetter
-import pareto
-from overshoot import*
-from risetime import*
 from plotgraphs import*
-from response import* 
+
 
 kp = 10
 tau = 45
 SP = 1
 E_0=0
-tfinal = 10
+tfinal = 100
 dt = 0.2
 t = np.arange(0, tfinal, dt)
 entries = len(t)
-print entries
-num = 7
+num =150
 x = np.zeros((entries,num))
 k_c = np.zeros(num)
 t_i = np.zeros(num)
@@ -59,8 +53,5 @@ for k in range(0,num):
 kc = k_c
 ti = t_i
 x = np.transpose(x)
-por = overshoot(x,SP,num,entries)
-tr = risetime(x,SP,num,entries,t)
-x = np.transpose(x)
-fig,ax1,ax2,ax3,line2,goodpoints = plotgraphs(k_c,t_i,tr,por,x,num,tfinal)
-fig = response(ax1,ax2,ax3,x,por,tr,kc,ti,goodpoints,line2,tfinal,dt,fig)
+fig = plotgraphs(kc,ti,x,num,entries,t,tfinal,dt,SP)
+
--- thirdorder.py
+++ thirdorder.py
@@ -4,7 +4,7 @@ from scipy import linalg
 from plotgraphs import*
 from ZN import*
 
-tfinal = 100 # simulation period
+tfinal = 100# simulation period
 dt = 0.2
 t = np.arange(0, tfinal, dt)
 entries = len(t)
@@ -15,23 +15,23 @@ t_i = np.zeros(num)
 por = np.zeros(num)
 tr = np.zeros(num)
 aa = np.random.rand(num,2)
-k_c =10*aa[:, 0]
-t_i = aa[:, 1] + 0.5
+k_c = (35 - 2)*aa[:, 0] +2
+t_i = 3.5*aa[:, 1]
 # coefficients of the transfer function Gp = kp/(s^3 + As^2 + Bs +C)
 A =3
 B = 3
 C = 1
 kp =0.125 
 SP = 1 
-#k_c = np.linspace(2,20,num)
+kcst = np.arange(0,35,dt)
 # Relatiopnship btwn kc and Ti obtained through the direct substitution method
-#t_i = np.abs(-kp*k_c/(((kp*k_c + C)/A)**2 - (kp*k_c + C)/A))
+tist =kp*kcst*A**2/(((A*B) - C - (kp*kcst))*(C + (kp*kcst)))
 kczn ,tizn  =ZN(A,B,C,kp) # Ziegler-Nichols settings via function ZN
 
 for k in range(0,num):
     if k==num-1:
         kc = kczn
-        ti = tizn    
+        ti = tizn
     else:    
         kc =k_c[k]
         ti = t_i[k]
@@ -45,7 +45,7 @@ for k in range(0,num):
     for i in range(0,entries):
         X = np.dot(1 - linalg.expm2(Amat*t[i]), Xo)
         x[i,k] = X[0]
-        if X[0] > 3*SP:
+        if X[0] > 2*SP:
             print 'the system is unstable, overshoot ratio and rise time undefined'
             x[:,k] = None
             break
@@ -54,6 +54,6 @@ for k in range(0,num):
 kc = k_c
 ti = t_i
 x = x.T
-fig = plotgraphs(kc,ti,x,num,entries,t,tfinal,dt,SP)
+fig = plotgraphs(kc,ti,x,num,entries,t,tfinal,dt,SP,kcst,tist)
 
 
